283cd30be448fed488c0ada487eaf6a342929991
Marcio Costa Rodrigues Junior <marciorodrigues.gft@sulamerica.com.br>
2022-05-18 14:39:38 -0300

    Define application

---
+++ src/main/java/br/com/examplejwt/header/HeaderJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.examplejwt.header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaderJwtApplication.class, args);
+	}
+
+}
